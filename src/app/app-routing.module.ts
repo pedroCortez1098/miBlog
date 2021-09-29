e3754f92bfb1b22c24872d55182662d223f77a98
Pedro Cortez <pedrocortez.fim@ms.uas.mx>
2021-09-29 17:50:39 -0600

    commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,9 @@ import { ContactComponent } from "./contact/contact.component";
 const routes: Routes = [
   { path: 'contact', component: ContactComponent },
   { path: 'articles', loadChildren: () => import('./articles/articles.module').then(m => m.ArticlesModule) },
+  { path: '', pathMatch: 'full', redirectTo: 'articles' },
+  { path: 'posts', loadChildren: () => import('./posts/posts.module').then(m => m.PostsModule) },
+  { path: '**', redirectTo: 'articles' }
 ];
 
 @NgModule({
--- src/app/articles/articles.component.ts
+++ src/app/articles/articles.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {ScullyRoute, ScullyRoutesService} from "@scullyio/ng-lib";
+
 
 @Component({
   selector: 'app-articles',
@@ -7,9 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ArticlesComponent implements OnInit {
 
-  constructor() { }
+  posts: ScullyRoute[] = [];
+  constructor(private scullyService: ScullyRoutesService) {
+
+  }
 
   ngOnInit(): void {
+    this.scullyService.available$.subscribe((posts)=>{
+      this.posts = posts.filter((posts) => posts.title);
+    });
   }
 
 }
