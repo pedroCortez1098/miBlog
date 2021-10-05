5f48d6d2cd7fd6c2e726e33b8a106e8a277c038a
Pedro Cortez <pedrocortez.fim@ms.uas.mx>
2021-10-05 14:25:26 -0600

    commit

---
+++ posts/angular-y-scully-io.md
@@ -0,0 +1,13 @@
+---
+title: 'Angular y ScullyIo'
+description: 'blog description'
+published: true
+---
+
+# Angular y ScullyIo
+Angular es un framework de JAvaScript robusto que podemos usar para crear aplicaciones web excelentes y de alto rendimiento.
+Scully es un popular generador de sitios web estáticos que potencia Angular con características Jamstack.
+Puede encontrar más sobre ellos en los siguientes enlaces:
+- https://angular.io
+- https://scully.io
+- https://www.jamstack.org
--- src/app/articles/articles.component.ts
+++ src/app/articles/articles.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {ScullyRoute, ScullyRoutesService} from "@scullyio/ng-lib";
+import {Subscription} from "rxjs";
 
 
 @Component({
@@ -7,17 +8,22 @@ import {ScullyRoute, ScullyRoutesService} from "@scullyio/ng-lib";
   templateUrl: './articles.component.html',
   styleUrls: ['./articles.component.scss']
 })
-export class ArticlesComponent implements OnInit {
+export class ArticlesComponent implements OnInit, OnDestroy {
 
   posts: ScullyRoute[] = [];
+  private routeSub: Subscription | undefined;
   constructor(private scullyService: ScullyRoutesService) {
 
   }
 
   ngOnInit(): void {
+    this.routeSub =
     this.scullyService.available$.subscribe((posts)=>{
       this.posts = posts.filter((posts) => posts.title);
     });
   }
 
+  ngOnDestroy() {
+    this.routeSub?.unsubscribe();
+  }
 }
